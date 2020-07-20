5b87c0a373f9716c6b4922c3bad232d6ab935bb4
Vinay0912 <68265300+Vinay0912@users.noreply.github.com>
2020-07-20 13:52:47 +0530

    day 4 all 8 assinments with tjs files

---
+++ day4 assingnment/day 4 assignment - 1/day 4 assingment.js
@@ -0,0 +1,16 @@
+console.log("4th day assingment ")
+for(i=1;i<=100;i++)
+{
+    if(i%3==0 && i%5==0){
+        console.log("fizzbuzz")
+    }
+    else if (i%3==0) {
+        console.log("fizz")
+    }
+    else if (i%5==0) {
+        console.log("buzz")
+    } 
+    
+    
+
+}
